49fdb9a4d4b54343e0a99345b86cdf7cb98b28a4
Yash Gupta <34357926+erYash15@users.noreply.github.com>
2020-08-07 17:01:05 +0530

    Add files via upload

--- dummy data/generate_dummy_data.py
+++ dummy data/generate_dummy_data.py
@@ -3,6 +3,7 @@ import sqlite3
 import names
 import random
 from random import sample
+from os import path
 
 Company = ['Microsoft','CocaCola','Samsung','Toyota','Dominos','Facebook','Apple','Google','Disney','Amazon']
 
@@ -12,39 +13,91 @@ Location = ['Delhi','Banglore','Mumbai','Kolkata','Madras','Hydrabad','Bhopal','
 
 Hobbies = ['Sports','Music','Dance','Reading','Gaming', 'Craft', 'Travel','Fishing','Acting','Gardening']
 
-try:
-    conn = sqlite3.connect('dummydata.db')
-    print("Open database successfully")
-except:
-    print("Open database unsuccessfully")
 
+# Connect to Database using sqlite3
+def connectdb(database):
+    '''
+    Input: String
+    Output: sqlite3.Connection
+    '''
 
-curr = conn.cursor()
-try:
-    curr.execute('''CREATE TABLE Data (
-        ID INTEGER PRIMARY KEY, 
-        Name	TEXT NOT NULL,
-        DOB	TEXT NOT NULL,
-        Location	TEXT NOT NULL,
-        Company	TEXT NOT NULL,
-        Hobbies	TEXT NOT NULL)''')
-    print ("Table created successfully")
-except:
-    print ("Table creation unsuccessfully.")
-    print ("Table exist already")
+    try:
+        conn = sqlite3.connect(database)
+        print("Open database successfully")
+        return conn
+    except:
+        print("Open database unsuccessfully")
+    
 
-conn.commit()
+def createtable(conn):
+    '''
+    Input: sqlite3.Connection
+    Output: Create the Table in the database
+    '''
 
-conn = sqlite3.connect('dummydata.db')
-try:
-    for i in range(2000):
-        curr = conn.cursor()
-        entry = ( i+1, names.get_full_name(), random.choice(DOB), random.choice(Location), random.choice(Company), \
-            ' '.join(sample(Hobbies, random.choice([1,2,3,4]))) )
-        strs = "INSERT INTO Data ( ID, Name, DOB, Location, Company, Hobbies) VALUES " + str(entry)
-        #print(strs)
-        curr.execute(strs)
-        curr.close()
+    curr = conn.cursor()
+    try:
+        curr.execute('''CREATE TABLE Data (
+            ID INTEGER PRIMARY KEY, 
+            Name	TEXT NOT NULL,
+            DOB	TEXT NOT NULL,
+            Location	TEXT NOT NULL,
+            Company	TEXT NOT NULL,
+            Hobbies	TEXT NOT NULL)''')
+        print ("Table created successfully")
+    except:
+        print ("Table creation unsuccessfully.")
+        print ("Table exist already")
     conn.commit()
-except:
-    conn.commit()    
+
+def inserttable(database, n_size):
+    '''
+    Input: database, size of database
+    Output: Insert the table with data
+    '''
+
+    con = sqlite3.connect(database)
+    try:
+        for i in range(n_size):
+            curr = con.cursor()
+            entry = ( i+1, names.get_full_name(), random.choice(DOB), random.choice(Location), random.choice(Company), \
+                ' '.join(sample(Hobbies, random.choice([1,2,3,4]))) )
+            strs = "INSERT INTO Data ( ID, Name, DOB, Location, Company, Hobbies) VALUES " + str(entry)
+            #print(strs)
+            curr.execute(strs)
+            curr.close()
+        con.commit()
+    except:
+        con.commit() 
+
+
+# driver program
+def main():
+    '''
+    Output: Creates the database with title - "dummydata_x" + where x represents number of datapoints.
+    '''
+    
+    # Data is set limited to 1000 points to avoid generating similar data.
+    while 1:
+        n_size = int(input("Please select size of database between 100 and 1000: "))
+        # Check limits
+        if n_size >= 100 and n_size <= 1000 :
+            break
+    
+    # Database making
+    database = "dummydata_" + str(n_size) + ".db"
+    print("Database is", database)
+    
+    # Check the database
+    if not path.exists(database):
+        # Connect to database    
+        conn = connectdb(database)
+        # Create Table
+        createtable(conn)
+        # Insert data into Table
+        inserttable(database, n_size)
+    else:
+        print("Database already exists.")
+    
+# Call the main function
+main()
